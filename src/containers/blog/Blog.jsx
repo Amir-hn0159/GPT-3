1f166a3fce9e12b3c9519fba640bae9ef919981a
amir <amir.hn0159@gmail.com>
2023-03-02 13:42:50 +0330

    footer created

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -5,9 +5,9 @@ import './blog.css'
 
 const Blog = () => {
   return (
-    <div className='gpt3__blog section__padding'>
+    <div className='gpt3__blog section__padding' id='blog'>
       <div className='gpt3__blog-heading'>
-        <h1 className='gradient__text'>A lot is happening, WE are bloging about it</h1>
+        <h1 className='gradient__text'>A lot is happening, We are bloging about it</h1>
       </div>
       <div className='gpt3__blog-container'>
         <div className='gpt3__blog-container_groupA'>
